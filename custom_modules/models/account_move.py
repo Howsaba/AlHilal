545ba250e71863d62a17a7ef8a0f9f69c48043e0
remon <remon.salem94@gmail.com>
2024-06-06 03:44:24 +0300

    [FIX]

--- custom_modules/models/account_move.py
+++ custom_modules/models/account_move.py
@@ -21,6 +21,6 @@ class HrPayslip(models.Model):
             rec.move_id.payment_method_selection = rec.employee_id.payment_method_selection
             line_id = rec.move_id.line_ids.filtered(lambda x:x.name == "Loan")
             partner_id = self.env['res.partner'].search([('employee_ids', 'in', rec.employee_id.id), ('name', '=', rec.employee_id.name)], limit=1)
-            raise UserError(f'/// {partner_id[0].name} ///')
-            line_id[0].partner_id = partner_id[0].id
+            if line_id and partner_id:
+                line_id[0].partner_id = partner_id[0].id
         return res
