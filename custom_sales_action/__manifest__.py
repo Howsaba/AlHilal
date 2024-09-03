cf9dd0ff2a4dc0de2f541736f21c79429172c563
remon <remon.salem94@gmail.com>
2024-09-03 11:52:10 +0300

    [MOD] custom_sales_action

--- custom_sales_action/__manifest__.py
+++ custom_sales_action/__manifest__.py
@@ -4,6 +4,7 @@
     'depends': ['sale','sale_subscription'],
     'data': [
         'views/sale_order_views.xml',
+        'views/sale_order_invoice_field.xml'
     ],
     'installable': True,
     'application': False,
