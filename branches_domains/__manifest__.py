321b3a9ee65b70ba633aa87ae05c3ce582159481
RanaOmarUqailahFaris <ranafaris90@gmail.com>
2024-09-23 16:42:15 +0300

    [ADD]branches

---
+++ branches_domains/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Branches Groups',
+    'version': '17.0.1.0.0',
+    'summary': 'Branches Groups',
+    'sequence': -1,
+    'depends': ['base', 'sale_subscription', 'hr','subscriptions_modification'],
+    'data': ["security/security.xml",
+        "views/res_users_branches.xml",
+             "views/edit_branches_views.xml"
+
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
