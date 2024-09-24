6525819e25bfcf93f000fe5ea956cabff01f446c
RanaOmarUqailahFaris <ranafaris90@gmail.com>
2024-09-24 16:33:13 +0300

    [ADD]branches_domain

---
+++ branches_domains/models/stock_picking_inherit.py
@@ -0,0 +1,26 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class SaleSubscriptionPlan(models.Model):
+    _inherit = "stock.picking"
+
+    branch_id = fields.Many2one('res.branch', string='Branch',
+                                domain=lambda self: [('id', 'in', self.env.user.branch_ids.ids)],
+                                default=lambda self: self.env.user.branch_id.id)
+
+
+
+    @api.onchange('branch_ids')
+    def _onchange_branch_ids(self):
+
+        for record in self:
+            if record.branch_ids:
+                return {
+                    'domain': {'branch_id': [('id', 'in', record.branch_ids.ids)]}
+                }
+            else:
+                return {
+                    'domain': {
+                        'branch_id': []}}
+
