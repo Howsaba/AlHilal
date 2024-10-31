d6806c0047f327e0009a29a4bfb7900963c54654
RanaOmarUqailahFaris <ranafaris90@gmail.com>
2024-10-31 17:29:25 +0300

    [MOD]alhilal_sale_subscription

--- alhilal_sale_subscription/models/sale_order_inherit.py
+++ alhilal_sale_subscription/models/sale_order_inherit.py
@@ -1,41 +1,36 @@
 from odoo import models, fields, api
 from datetime import timedelta
 
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    end_date = fields.Date(compute='_compute_end_date', store=True,readonly=True)
+    end_date = fields.Date(compute='_compute_end_date', store=True, readonly=True)
 
     @api.depends('start_date', 'plan_id.billing_period_days')
     def _compute_end_date(self):
         for record in self:
             if record.start_date and record.plan_id and record.plan_id.billing_period_days:
                 billing_period_days = record.plan_id.billing_period_days
-                record.end_date = record.start_date + timedelta(days=billing_period_days-1)
+                record.end_date = record.start_date + timedelta(days=billing_period_days - 1)
             else:
-                False
+                record.end_date = False  # Correctly assign False to end_date
 
     @api.model
     def create(self, vals):
         res = super(SaleOrder, self).create(vals)
-
         res._compute_end_date()
-
         return res
+
     def _create_invoices(self, grouped=False, final=False, date=None):
-        # Call the original method to create invoices
         moves = super(SaleOrder, self)._create_invoices(grouped=grouped, final=final, date=date)
 
-        # After creating the invoices, update the deferred_end_date in invoice lines
         for order in self:
+            order._compute_end_date()
+
             for invoice in moves:
-                # Ensure that the sale order and invoice are related
                 if invoice.invoice_origin == order.name:
-                    # Loop through the invoice lines and update the deferred_end_date
                     for line in invoice.invoice_line_ids:
-                        # Check if the deferred_end_date field exists
-                        if hasattr(line, 'deferred_end_date'):
-                            # Update deferred_end_date with the sale order's end_date
                             line.deferred_end_date = order.end_date
                             line.name = line.product_id.name
 
