8f6472bb47467406477cac7e1404a45bf4a547c1
remon <remon.salem94@gmail.com>
2024-11-07 14:37:33 +0200

    [REM]

--- custom_modules/models/account_move.py
+++ custom_modules/models/account_move.py
@@ -12,15 +12,15 @@ class AccountMove(models.Model):
          ('cheque', 'Cheque')])
 
 
-class HrPayslip(models.Model):
-    _inherit = "hr.payslip"
-
-    def action_payslip_done(self):
-        res = super().action_payslip_done()
-        for rec in self:
-            rec.move_id.payment_method_selection = rec.employee_id.payment_method_selection
-            line_id = rec.move_id.line_ids.filtered(lambda x:x.name == "Loan")
-            partner_id = self.env['res.partner'].search([('employee_ids', 'in', rec.employee_id.id)], limit=1)
-            if line_id and partner_id:
-                line_id[0].partner_id = partner_id[0].id
-        return res
+# class HrPayslip(models.Model):
+#     _inherit = "hr.payslip"
+#
+#     def action_payslip_done(self):
+#         res = super().action_payslip_done()
+#         for rec in self:
+#             rec.move_id.payment_method_selection = rec.employee_id.payment_method_selection
+#             line_id = rec.move_id.line_ids.filtered(lambda x:x.name == "Loan")
+#             partner_id = self.env['res.partner'].search([('employee_ids', 'in', rec.employee_id.id)], limit=1)
+#             if line_id and partner_id:
+#                 line_id[0].partner_id = partner_id[0].id
+#         return res
