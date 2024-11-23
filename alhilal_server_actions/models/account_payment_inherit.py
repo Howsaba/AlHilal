ef890433b09ccaa18d293548809f17b795fa9d73
RanaOmarUqailahFaris <ranafaris90@gmail.com>
2024-11-23 12:16:51 +0300

    [MOD]alhilal_server_actions,server_action_cancel_records

--- alhilal_server_actions/models/account_payment_inherit.py
+++ alhilal_server_actions/models/account_payment_inherit.py
@@ -11,3 +11,8 @@ class AccountPayment(models.Model):
                 payment.state = 'draft'
 
 
+
+    def cancel_records(self):
+        for record in self:
+            if record.state == 'draft':
+                record.action_cancel()
