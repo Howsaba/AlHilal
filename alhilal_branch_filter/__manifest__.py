c0ccacfb247a8acbfdc7bca9ca3e500c25f4eccc
RanaOmarUqailahFaris <ranafaris90@gmail.com>
2025-03-09 10:14:18 +0300

    [ADD]branch_filter

---
+++ alhilal_branch_filter/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Customiz branch filter",
+    "summary": "Customize Partner branch filter ",
+    "description": """
+        Customize Partner branch filter "
+    """,
+    "author": "Rana Faris",
+    "website": "",
+    "version": "0.1",
+    "depends": ["base", "account_accountant"],
+    'data': [
+             'views/account_move_line_filter_branch.xml',
+    ],
+}
