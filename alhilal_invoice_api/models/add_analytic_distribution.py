26e005cd8bd3ffc70042e47ca74a487686e25f90
3safiber <salameha595@gmail.com>
2025-04-16 15:29:02 +0300

    [MOD]alhilal_invoice_api

--- alhilal_invoice_api/models/add_analytic_distribution.py
+++ alhilal_invoice_api/models/add_analytic_distribution.py
@@ -4,20 +4,20 @@ from odoo import fields, models
 class AnalyticDistribution(models.Model):
     _inherit = "res.branch"
 
-    # sales_analytic_distributions = fields.One2many(
-    #     "account.analytic.account",
-    #     "branch_id",
-    #     string="Sales Analytic Distributions",
-    #     required=True,
-    # )
-    sales_analytic_distributions = fields.Many2many(
+    sales_analytic_distributions = fields.One2many(
         "account.analytic.account",
+        "branch_id",
         string="Sales Analytic Distributions",
         required=True,
     )
+    # sales_analytic_distributions = fields.Many2many(
+    #     "account.analytic.account",
+    #     string="Sales Analytic Distributions",
+    #     required=True,
+    # )
 
 
-# class AccountAnalyticDistribution(models.Model):
-#     _inherit = "account.analytic.account"
+class AccountAnalyticDistribution(models.Model):
+    _inherit = "account.analytic.account"
 
-#     branch_id = fields.Many2one("res.branch", string="Branch")
+    branch_id = fields.Many2one("res.branch", string="Branch")
